4939907e0969eb7e19488f826f213dcbc60eb64a
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2019-03-09 12:40:26 -0500

    Función para listar los mensajes enviados

--- server/src/controllers/mensajeController.js
+++ server/src/controllers/mensajeController.js
@@ -37,7 +37,26 @@ controller.obtenerMensajesRecibidos = async (req, res) => {
 
   if (req.params.pagina) pagina = req.params.pagina;
 
-  Mensaje.find({ recibido: req.usuario.sub }).populate('emitido').paginate(pagina, elementosPorPagina, (error, mensajes, total) => {
+  Mensaje.find({ recibido: req.usuario.sub }).populate('emitido', 'nombres apellidos nombreUsuario imagen _id').paginate(pagina, elementosPorPagina, (error, mensajes, total) => {
+    if (error) return res.status(500).json({ mensaje: 'Error en el servidor' });
+
+    if (!mensajes) return res.status(404).json({ mensaje: 'No existen mensajes' });
+
+    return res.status(200).json({
+      total,
+      paginas: Math.ceil(total/elementosPorPagina),
+      mensajes
+    });
+  });
+}
+
+controller.obtenerMensajesEnviados = async (req, res) => {
+  let pagina = 1;
+  let elementosPorPagina = 4;
+
+  if (req.params.pagina) pagina = req.params.pagina;
+
+  Mensaje.find({ emitido: req.usuario.sub }).populate('emitido recibido', 'nombres apellidos nombreUsuario imagen _id').paginate(pagina, elementosPorPagina, (error, mensajes, total) => {
     if (error) return res.status(500).json({ mensaje: 'Error en el servidor' });
 
     if (!mensajes) return res.status(404).json({ mensaje: 'No existen mensajes' });
--- server/src/routes/mensajeRoutes.js
+++ server/src/routes/mensajeRoutes.js
@@ -5,5 +5,6 @@ const { asegurarAutenticacion } = require('../libs/jwt');
 
 router.post('/mensaje', asegurarAutenticacion, MensajeController.guardarMensaje);
 router.get('/misMensajes/:pagina?', asegurarAutenticacion, MensajeController.obtenerMensajesRecibidos);
+router.get('/mensajes/:pagina?', asegurarAutenticacion, MensajeController.obtenerMensajesEnviados);
 
 module.exports = router;
